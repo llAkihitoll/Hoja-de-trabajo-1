19b9a740d336a78a7c16373e9e099d7a1bd96be7
Oliver <161040825+danifig11@users.noreply.github.com>
2025-01-21 20:26:30 -0600

    llenar, funciones de velocidades, vaciar y servir

--- LicuadoraEnSi.java
+++ LicuadoraEnSi.java
@@ -30,3 +30,66 @@ public class LicuadoraEnSi implements Licuadora {
     public boolean estaEncendida() {
         return encendida;
     }
+    
+    @Override
+    public double llenar(double gramos) {
+        if (gramos <= 0) {
+            return -1; // Error: peso no válido
+        }
+        if (contenido + gramos > MAX_GRAMOS) {
+            return -2; // Error: pasa la capacidad máxima
+        }
+        contenido += gramos;
+        return contenido; // Muestra el peso actual
+    }
+
+    @Override
+    public int incrementarVelocidad() {
+        if (!encendida) {
+            return -1; // No se puede cambiar la velocidad si está apagada
+        }
+        if (velocidad < MAX_VELOCIDAD) {
+            velocidad++;
+            return velocidad;
+        }
+        return -2; // Ya está en la velocidad máxima
+    }
+
+    @Override
+    public int decrementarVelocidad() {
+        if (!encendida) {
+            return -1; // No se puede cambiar la velocidad si está apagada
+        }
+        if (velocidad > 0) {
+            velocidad--;
+            return velocidad;
+        }
+        return -2; // Ya está en la velocidad mínima
+    }
+
+    @Override
+    public int consultarVelocidad() {
+        return velocidad;
+    }
+
+    @Override
+    public boolean estarLlena() {
+        return contenido >= MAX_GRAMOS;
+    }
+
+    @Override
+    public double vaciar() {
+        double gramosVaciados = contenido;
+        contenido = 0.0;
+        return gramosVaciados;
+    }
+
+    @Override
+    public double servir(double gramosRestados) {
+        if (gramosRestados <= 0 || gramosRestados > contenido) {
+            return -1;
+        }
+        contenido -= gramosRestados;
+        return contenido; // Devuelve el peso que queda
+    }
+}
