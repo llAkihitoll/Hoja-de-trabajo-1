7045af2cc737cab8550e3f22fe781e25a8c5198e
llAkihitoll <saul.esteban.casti@gmail.com>
2025-02-03 10:48:50 -0600

    Licuadora.java

--- Licuadora.java
+++ Licuadora.java
@@ -1,12 +1,1 @@
-public interface Licuadora {
-    void encender(); 
-    void apagar(); 
-    boolean estaEncendida();
-    double llenar(double volumen); 
-    int incrementarVelocidad();
-    int decrementarVelocidad();
-    int consultarVelocidad();
-    boolean estarLlena();
-    double vaciar();
-    double servir(double volumenRestado);
-}
+
